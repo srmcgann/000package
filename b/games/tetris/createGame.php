6903678c643dee981f9e3f8e2d15ca8fbe0d769c
scott mcgann <s.r.mcgann@hotmail.com>
2023-11-21 21:15:47 +0000

    sync

--- b/games/tetris/createGame.php
+++ b/games/tetris/createGame.php
@@ -5,13 +5,14 @@
   if($gamedata != "null"){
     do{
       $id = rand(getrandmax() - 1e8, getrandmax());
-      $sql = "SELECT id FROM games WHERE id = " . $id;
+      $sql = "SELECT id FROM tetrisGames WHERE id = " . $id;
       $res = mysqli_query($link, $sql);
       $good = true;
       if(mysqli_num_rows($res)) $good = false;
     }while(!$good);
-    $sql = 'INSERT INTO games (gamedataA, gamedataB, gamedataC, gamedataD, id) VALUES("'.$gamedata.'", "", "", "", ' . $id . ')';
+    $sql = 'INSERT INTO tetrisGames (gamedataA, gamedataB, gamedataC, gamedataD, id) VALUES("'.$gamedata.'", "", "", "", ' . $id . ')';
     mysqli_query($link, $sql);
     echo json_encode([mysqli_insert_id($link)]);
   }
 ?>
+
